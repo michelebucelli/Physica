35eebb46da6d9667971319968303bf0e7e289b42
buch415 <michelebucelli@alice.it>
2014-02-18 20:02:12 +0100

    Added basic dialog functionality

--- bin/data/ui/js/dialogs.js
+++ bin/data/ui/js/dialogs.js
@@ -1,10 +1,22 @@
 //Script functions to show dialogs
 
-function textDialog (control, title, prompt) {
+//Shows a text dialog
+function textDialog (control, title, prompt, after) {
 	var dialog = control.addChild("(@ui)/textDialog.xml");
 	
 	dialog.content.title.text = title;
 	dialog.content.prompt.text = prompt;
 	
+	dialog.userDefined.after = after;
+	
 	dialog.grabEvents();
 };
+
+//Returns to the caller
+function dialogReturn (dialog, result) {
+	dialog.releaseEvents();
+	dialog.parent.removeChild(dialog.id);
+	
+	dialog.parent.userDefined.dialogResult = result;
+	dialog.parent.scriptTimeout ( 0, dialog.userDefined.after );
+}
